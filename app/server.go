54ffec5a246e07217b9918d44a4bcb0a5e7c3497
ThunderSmoker <pitlawararadhya@gmail.com>
2024-05-29 19:02:53 +0530

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -1,48 +1,100 @@
 package main
-
 import (
-	"fmt"
-	"net"
-	"os"
+	"sync"
 )
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	for {
-		// Buffer to store data
-		buf := make([]byte, 1024)
-		//print command received
-		xtring, err := conn.Read(buf)
-		
-		fmt.Println("Received:", string(buf[:xtring]))
-		if err != nil {
-			fmt.Println("Error reading from connection:", err.Error())
-			return
-		}
-		// Respond with PONG
-		conn.Write([]byte("+PONG\r\n"))
-	}
+var Handlers = map[string]func([]Value) Value{
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hset,
+	"HGET":    hget,
+	"HGETALL": hgetall,
+	"ECHO":    echo,
 }
-
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379:", err.Error())
-		os.Exit(1)
+func ping(args []Value) Value {
+	if len(args) == 0 {
+		return Value{typ: "string", str: "PONG"}
 	}
-	defer l.Close()
-
-	fmt.Println("Listening on port 6379...")
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection:", err.Error())
-			continue
-		}
-		go handleConnection(conn) // Handle each connection in a new goroutine
+	return Value{typ: "string", str: args[0].bulk}
+}
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
 	}
+	key := args[0].bulk
+	value := args[1].bulk
+	SETsMu.Lock()
+	SETs[key] = value
+	SETsMu.Unlock()
+	return Value{typ: "string", str: "OK"}
+}
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'get' command"}
+	}
+	key := args[0].bulk
+	SETsMu.RLock()
+	value, ok := SETs[key]
+	SETsMu.RUnlock()
+	if !ok {
+		return Value{typ: "null"}
+	}
+	return Value{typ: "bulk", bulk: value}
+}
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+	HSETsMu.Lock()
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = value
+	HSETsMu.Unlock()
+	return Value{typ: "string", str: "OK"}
+}
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
+	}
+	hash := args[0].bulk
+	key := args[1].bulk
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
+	if !ok {
+		return Value{typ: "null"}
+	}
+	return Value{typ: "bulk", bulk: value}
+}
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hgetall' command"}
+	}
+	hash := args[0].bulk
+	HSETsMu.RLock()
+	value, ok := HSETs[hash]
+	HSETsMu.RUnlock()
+	if !ok {
+		return Value{typ: "null"}
+	}
+	values := []Value{}
+	for k, v := range value {
+		values = append(values, Value{typ: "bulk", bulk: k})
+		values = append(values, Value{typ: "bulk", bulk: v})
+	}
+	return Value{typ: "array", array: values}
+}
+func echo(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'echo' command"}
+	}
+	return Value{typ: "bulk", bulk: args[0].bulk}
 }
